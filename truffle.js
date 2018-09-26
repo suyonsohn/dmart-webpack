d9db6b97130756192de161a444b746ac9f157ce6
sys <suyonsohn@gmail.com>
2018-09-26 16:41:59 -0700

    Migrate EStore contract

--- truffle.js
+++ truffle.js
@@ -3,10 +3,10 @@ require('babel-register')
 
 module.exports = {
     networks: {
-        ganache: {
-            host: '127.0.0.1',
+        development: {
+            host: "127.0.0.1",
             port: 8545,
-            network_id: '*' // Match any network id
+            network_id: "*" // Match any network id
         }
     }
-}
+};
